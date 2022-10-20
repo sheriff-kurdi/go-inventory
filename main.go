6ba5849b73b64d054f8e9d637ab01e60a2832940
Kurdi <sheriff.kurdi@gmail.com>
2022-10-20 16:33:29 +0200

    cors

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 
 	// Routes
 	app.Use(cors.New(cors.Config{
-		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowHeaders:     "*",
 		AllowOrigins:     "*",
 		AllowCredentials: true,
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
