b3a1e3b21e7b67aa3624747f6a0cbcaae847770c
sheriff-kurdi <sheriff.kurdi@gmail.com>
2023-03-18 16:49:57 +0200

    refac

---
+++ infrastructure/repositories/sqlserver/products.repository.go
@@ -0,0 +1,22 @@
+package sqlserver
+
+import (
+	"kurdi-go/core/entities/products"
+	"kurdi-go/infrastructure/database/postgres"
+)
+
+type ProductsRepository struct {
+}
+
+func NewProductsRepository() ProductsRepository {
+	repository := ProductsRepository{}
+	return repository
+}
+
+func (repository ProductsRepository) SelectAll() []products.Product {
+	var products []products.Product
+	query := `SELECT * FROM products ;`
+
+	postgres.Connect().Raw(query).Scan(&products)
+	return products
+}
