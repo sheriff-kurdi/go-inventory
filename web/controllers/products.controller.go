d82139c42c978d0e1000b739e30d3ef6f3d9cb65
sheriff-kurdi <sheriff.kurdi@gmail.com>
2023-04-05 13:46:54 +0200

    swaggert

--- web/controllers/products.controller.go
+++ web/controllers/products.controller.go
@@ -3,9 +3,7 @@ package controllers
 import (
 	"kurdi-go/core/services"
 	"kurdi-go/web/resources"
-	"kurdi-go/web/utils"
 	"strconv"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -30,32 +28,6 @@ func NewProductsController() *ProductsController {
 // @Security ApiKeyAuth
 // @Router /api/v1/products [get]
 func (controller ProductsController) GetAll(ctx *fiber.Ctx) error {
-	//get all
-	//s := products.Product{}
-	// Get now time.
-	now := time.Now().Unix()
-
-	// Get claims from JWT.
-	claims, err := utils.ExtractTokenMetadata(ctx)
-	if err != nil {
-		// Return status 500 and JWT parse error.
-		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": true,
-			"msg":   err.Error(),
-		})
-	}
-
-	// Set expiration time from JWT data of current book.
-	expires := claims.Expires
-
-	// Checking, if now time greather than expiration from JWT.
-	if now > expires {
-		// Return status 401 and unauthorized error message.
-		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"error": true,
-			"msg":   "unauthorized, check expiration time of your token",
-		})
-	}
 	languageCode := ctx.Query("language_code")
 	products := controller.productsService.ListAll(languageCode)
 	response := resources.Ok(products, "")
@@ -69,6 +41,7 @@ func (controller ProductsController) GetAll(ctx *fiber.Ctx) error {
 // @Accept json
 // @Produce json
 // @Security ApiKeyAuth
+// @Param id path string true "Product Id"
 // @Success 200 {object} vm.ProductVM
 // @Security ApiKeyAuth
 // @Router /api/v1/products/{id} [get]
